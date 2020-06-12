1b8fbd5dcef1683e6f5142ba1969fc665a6a1aaa
Yang Wang <penguinwang@smail.nchu.edu.tw>
2020-06-12 10:22:46 +0800

    Update vec_from_val.R

--- vec_from_val.R
+++ vec_from_val.R
@@ -1,20 +1,25 @@
 A = matrix(c(1, 1, -1, 1, 3, 1, -1, 1, 3), 3, 3)
 
 vec_from_val = function(A){
+  # A should be Hermitian matrix
   n = sqrt(length(A))
   Aeig = eigen(A)$values
+  # V is eigenvecters matrix
   V = matrix(ncol=n, nrow=n)
   
   for (i in 1:n){
-    AM = Aeig[-1]
+    AM = Aeig[-i]
     for (j in 1:n){
+      # Minors matrix B
       B = A[-j, ]
       B = B[, -j]
       Beig = eigen(B)$values
+
       down = 1; up = 1
-      n0 = n - 1
+      # n_0 is the dimension of B
+      n_0 = n - 1
       
-      for (k in 1:n0){
+      for (k in 1:n_0){
         down = down * (Aeig[i] - AM[k])
         up = up * (Aeig[i] - Beig[k])
       }
